96ff8bedd3fb5ecf109731715f69e999b8364914
Amanda Mitchell <amandamitchell707@gmail.com>
2018-09-07 18:04:00 -0700

    Work on active_record_lite project

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -1,26 +1,48 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+
+    query_result = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        '#{self.table_name}'
+    SQL
+
+    @columns = query_result[0].map(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |column|
+
+      define_method(column) do
+        instance_variable_get("@#{column}")
+      end
+
+      define_method("#{column}=") do |value|
+        instance_variable_set("@#{column}", value)
+      end
+
+      #@attributes[key] = val
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.to_s.tableize
   end
 
   def self.all
-    # ...
+    @sql_objects ||= []
   end
 
   def self.parse_all(results)
@@ -32,11 +54,12 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    self.class.all << self
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
+    @attributes
   end
 
   def attribute_values
